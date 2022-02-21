29ae056670bf9eb4b6c9b2271069470bf33958a6
RAHUL R <88367811+r-rahul1@users.noreply.github.com>
2022-02-21 12:32:30 +0530

    Time: 209 ms (57.45%), Space: 15.5 MB (43.74%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,8 +1,13 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        '''
         dic = Counter(nums)
         l = len(nums)
         for num,freq in dic.items():
             if freq >= l/2:
                 return num
+        '''
+        nums.sort()
+        return nums[len(nums)//2]
+        
