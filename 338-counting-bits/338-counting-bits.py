4a48ccaccccc7bfa77c10f81d727876e65aa6a67
RAHUL R <88367811+r-rahul1@users.noreply.github.com>
2022-03-01 07:52:10 +0530

    Time: 179 ms (28.59%), Space: 25.3 MB (20.83%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,7 +1,25 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res=[]
-        for i in range(n+1):
-            temp=(bin(i).replace("0b",""))
-            res.append(temp.count('1'))
+        dic = {}
+        def fun(num):
+            if num == 0:
+                return 0
+            elif num == 1:
+                return 1
+            
+            elif num in dic:
+                return dic[num]
+            
+            if num % 2 == 0:
+                dic[num] = fun(num//2)
+            else:
+                dic[num] = 1 + fun(num//2)
+            return dic[num]
+        
+        res=[None]*(n+1)
+        
+        for i in range(len(res)):
+            res[i] = fun(i)
+        
         return res
+        
