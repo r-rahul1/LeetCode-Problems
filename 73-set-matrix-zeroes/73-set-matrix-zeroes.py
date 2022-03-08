844f8069d0efd1d9e9e195d4fefe0be888902d1b
Rahul Raghunathan <88367811+r-rahul1@users.noreply.github.com>
2022-03-09 01:05:34 +0530

    Time: 205 ms (37.63%), Space: 14.7 MB (73.23%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,6 +3,32 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        rz = False
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        rz = True
+                    matrix[0][j] = 0
+                    
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0  
+            
+        if rz:
+            for i in range(len(matrix[0])):
+                matrix[0][i] = 0
+                           
+        
+        '''
+        #Space complexity -> O(m+n)
         def fun(arr,rc):
             for x,y in rc:
                 for i in range(len(matrix)):
@@ -17,3 +43,4 @@ class Solution:
                     rc.append((i,j)) 
         
         fun(matrix,rc)
+        '''
