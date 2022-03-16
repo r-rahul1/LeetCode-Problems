776aceca06cdfda7c582ed18991a7c48ffaee3c3
Rahul Raghunathan <88367811+r-rahul1@users.noreply.github.com>
2022-03-16 10:58:02 +0530

    Time: 1844 ms (71.61%), Space: 18.4 MB (17.03%) - LeetHub

--- 1993-operations-on-tree/1993-operations-on-tree.py
+++ 1993-operations-on-tree/1993-operations-on-tree.py
@@ -2,27 +2,45 @@ class LockingTree:
 
     def __init__(self, parent: List[int]):
         self.parent = parent
-        self.dic = {x:[None,[]] for x in range(len(parent))}
-        self.dic[0] = [None,[]]
-        #self.count = 0
+        self.dic = {}
+        self.children = {}
+        self.dic[0] = [None,0]
         for x in range(1,len(parent)):
-            self.dic[parent[x]][1].append(x)
+            self.dic[x] = [None,0]
+            if x not in self.children:
+                self.children[x] = []
+            if parent[x] in self.children:
+                self.children[parent[x]].append(x)
+            else:
+                self.children[parent[x]] = [x]
 
     def lock(self, num: int, user: int) -> bool:
         if self.dic[num][0]:
             return False
+        
         self.dic[num][0] = user
+        acending = self.parent[num]
+        while acending != -1:
+            self.dic[acending][1] += 1
+            acending = self.parent[acending]
+        
         return True
-
+    
     def unlock(self, num: int, user: int) -> bool:
         if not self.dic[num][0] or self.dic[num][0] != user:
             return False
+        
         self.dic[num][0] = None
+        acending = self.parent[num]
+        while acending != -1:
+            self.dic[acending][1] -= 1
+            acending = self.parent[acending]
+        
         return True
 
     def upgrade(self, num: int, user: int) -> bool:
         
-        def check():
+        def check(num):
             #checking ancestors
             acending = self.parent[num]
             while acending != -1:
@@ -30,21 +48,21 @@ class LockingTree:
                     return False
                 acending = self.parent[acending]
             
-            self.count = 0
             #checking and unlocking decendants
-
             def fun(temp):
-                for x in self.dic[temp][1]:
-                    if self.dic[x][0]:
-                        self.count += 1
-                        self.dic[x][0] = None
-                    fun(x)
+                for x in self.children[temp]:
+                    self.dic[x][1] = 0
+                    self.dic[x][0] = None
+                    if self.children[x]:
+                        fun(x)
+                    
             fun(num)        
-            return True if self.count != 0 else False
+            return True
                 
-        if self.dic[num][0] or not check():
+        if self.dic[num][0] or self.dic[num][1] == 0 or not check(num):
             return False        
         
+        self.dic[num][1] = 0
         self.dic[num][0] = user
         return True
 
