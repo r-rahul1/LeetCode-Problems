ba0b18bc3072709f1bd74841338ee58334886753
Rahul Raghunathan <88367811+r-rahul1@users.noreply.github.com>
2022-03-17 10:59:08 +0530

    Time: 62 ms (9.21%), Space: 13.9 MB (30.17%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -1,5 +1,16 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
+        ans = curr = 0
+        for i,ch in enumerate(s):
+            if ch == '(':
+                curr += 1
+            else:
+                curr -= 1
+                if s[i-1] == '(':
+                    ans += 1<<curr
+        return ans
+        
+        '''
         stack = [0]
         
         for ch in s:
@@ -10,3 +21,4 @@ class Solution:
                 stack[-1] += max(2*temp,1)
         
         return stack.pop()
+        '''
